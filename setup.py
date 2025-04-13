4dbc742a827a27d2559c22f3f99a39d46a0ab84c
Xinyi Deng <143345699+ppxinyi@users.noreply.github.com>
2025-04-12 22:58:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dge',
-    version='0.1.0',
-    author='Xinyi Deng',
-    description='A toolkit for differential gene expression analysis',
+    name="DGE",
+    version="0.1.0",
+    author="Xinyi Deng",
+    author_email="your_email@example.com",
+    description="A tool for RNA-seq differential gene expression analysis",
     packages=find_packages(),
     install_requires=[
-        'pandas',
-        'matplotlib',
-        'seaborn',
-        'rpy2',
-        'scikit-learn',
-        'numpy'
+        "numpy",
+        "pandas",
+        "seaborn",
+        "matplotlib",
+        "scipy",
+        "scikit-learn",
     ],
     entry_points={
-        'console_scripts': [
-            'dge=dge.cli:main',
-        ],
+        "console_scripts": [
+            "dge-run = DGE.main:main"
+        ]
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.7",
 )
